566cfd2a74f6bbe088775edf3658e67a107c603c
tracer-ysyx <tracer@ysyx.org>
2024-08-22 13:53:54 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:53:54 up  2:05,  1 user,  load average: 0.26, 0.15, 0.10

--- nemu/src/monitor/sdb/sdb.c
+++ nemu/src/monitor/sdb/sdb.c
@@ -47,10 +47,16 @@ static int cmd_c(char *args) {
   return 0;
 }
 
-
 static int cmd_q(char *args) {
   nemu_state.state = NEMU_QUIT;
   return -1;
+}
+
+static int cmd_si(char *args) {
+  char **endptr = NULL;
+  unsigned long long num_steps = strtoull(args, endptr, 0);
+  cpu_exec(num_steps);
+  return 0;
 }
 
 static int cmd_help(char *args);
@@ -63,6 +69,7 @@ static struct {
   { "help", "Display information about all supported commands", cmd_help },
   { "c", "Continue the execution of the program", cmd_c },
   { "q", "Exit NEMU", cmd_q },
+  { "si", "Step instruction", cmd_si },
 
   /* TODO: Add more commands */
 
