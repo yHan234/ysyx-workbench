f3cae8959a6d7ef630fd40e6e7ff8c85ddd9cc40
tracer-ysyx <tracer@ysyx.org>
2024-08-22 16:19:36 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  16:19:36 up  4:31,  1 user,  load average: 0.18, 0.16, 0.11

--- nemu/src/isa/riscv32/reg.c
+++ nemu/src/isa/riscv32/reg.c
@@ -25,9 +25,9 @@ const char *regs[] = {
 
 void isa_reg_display() {
   extern CPU_state cpu;
-  printf("pc\t\t%#08x\n", cpu.pc);
+  printf("pc\t\t0x%08x\n", cpu.pc);
   for (int i = 0; i < 32; ++i) {
-    printf("%s\t\t%#010x\n", regs[i], cpu.gpr[i]);
+    printf("%s\t\t0x%08x\n", regs[i], cpu.gpr[i]);
   }
 }
 
