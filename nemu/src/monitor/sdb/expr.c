25168496b19ee80097133f7ab0a956f5cc4887e3
tracer-ysyx <tracer@ysyx.org>
2024-08-23 23:06:56 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:06:56 up 1 day,  2:35,  2 users,  load average: 0.09, 0.09, 0.13

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -152,7 +152,7 @@ static bool is_paren_paring(int bo, int eo) {
 
 // evaluate
 
-#define FAIL(format, ...) do { printf(format, __VA_ARGS__); *success = false; return 0; } while(0)
+#define EVAL_FAIL(format, ...) do { printf("eval: " format, __VA_ARGS__); *success = false; return 0; } while(0)
 #define INDICATE_FMT " at position %d\n%s\n%*.s^\n"
 #define INDICATE_ARG(str, pos) pos, str, pos, ""
 #define INDICATE_EXPR_ARG(pos) pos, expression, pos, ""
@@ -164,7 +164,7 @@ static word_t tk_to_int(Token tk, bool *success) {
 
   if (errno == ERANGE) {
     errno = 0;
-    FAIL("eval: The number is too long" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos));
+    EVAL_FAIL("The number is too long" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos));
   } else {
     Log("Terminal integer: %u" INDICATE_FMT, num, INDICATE_EXPR_ARG(tk.pos));
     return num;
@@ -176,7 +176,7 @@ static word_t tk_reg_get(Token tk, bool *success) {
   bool local_success = true;
   wchar_t num = isa_reg_str2val(tk.str + 1, &local_success);
   if (!local_success) {
-    FAIL("eval: Wrong register name %s" INDICATE_FMT, tk.str, INDICATE_EXPR_ARG(tk.pos));
+    EVAL_FAIL("Wrong register name %s" INDICATE_FMT, tk.str, INDICATE_EXPR_ARG(tk.pos));
   } else {
     Log("Terminal register: %s: 0x%08x" INDICATE_FMT, tk.str, num, INDICATE_EXPR_ARG(tk.pos));
     return num;
@@ -194,14 +194,15 @@ static word_t eval(bool *success, int bo, int eo) {
     } else if (tk_is_reg(tk)) {
       return tk_reg_get(tk, success);
     } else {
-      FAIL("eval: Expect a number or a register" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos + 1));
+      EVAL_FAIL("Expect a number or a register" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos + 1));
     }
   } else if (is_paren_paring(bo, eo)) {
     // remove the parentheses.
+    Token tk = tokens[bo];
     if (bo + 1 == eo - 1) {
-      FAIL("eval: Expressions should be in parentheses" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[bo].pos));
+      EVAL_FAIL("Expressions should be in parentheses" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos));
     } else {
-      Log("Enter parentheses" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[bo].pos));
+      Log("Enter parentheses" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos));
       return eval(success, bo + 1, eo - 1);
     }
   } else {
@@ -240,28 +241,29 @@ static word_t eval(bool *success, int bo, int eo) {
 
     // If there is no binary operator, then the beginning must be a unary operator
     if (op == OP_NOTOP) {
-      if (tk_is_op(tokens[bo]) && op_is_unary(op = tk_to_op(tokens[bo], true))) {
-        Log("Process unary operator %s" INDICATE_FMT, tokens[bo].str, INDICATE_EXPR_ARG(tokens[bo].pos));
-        if (bo + 1 >= eo) FAIL("eval: Expect a expression" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[bo].pos + 1));
+      Token tk = tokens[bo];
+      if (tk_is_op(tk) && op_is_unary(op = tk_to_op(tk, true))) {
+        Log("Process unary operator %s" INDICATE_FMT, tk.str, INDICATE_EXPR_ARG(tk.pos));
+        if (bo + 1 >= eo) EVAL_FAIL("Expect a expression" INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos + 1));
         word_t x = eval(success, bo + 1, eo), res;
         switch (op)
         {
         case OP_POS   : res = x; break;
         case OP_NEG   : res = ~x + 1; break;
         case OP_DEREF : res = vaddr_read(x, 4); break;
-        default       : panic("eval: Invalid unary operator");
+        default       : panic("eval internal error: Invalid unary operator");
         }
-        Log("Calculate: %s%u => %u" INDICATE_FMT, tokens[bo].str, x, res, INDICATE_EXPR_ARG(tokens[bo].pos));
+        Log("Calculate: %s%u => %u" INDICATE_FMT, tk.str, x, res, INDICATE_EXPR_ARG(tk.pos));
         return res;
       } else {
-        FAIL("eval: Invalid expression"  INDICATE_FMT, INDICATE_EXPR_ARG(tokens[bo].pos));
+        EVAL_FAIL("Invalid expression"  INDICATE_FMT, INDICATE_EXPR_ARG(tk.pos));
       }
     }
 
     // there is a binary operator
     Log("Divide from binary operator %s" INDICATE_FMT, tokens[p].str, INDICATE_EXPR_ARG(tokens[p].pos));
-    if (bo >= p) FAIL("eval: Expect a expression" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[bo].pos));
-    if (p + 1 >= eo) FAIL("eval: Expect a expression" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[p + 1].pos + 1));
+    if (bo >= p) EVAL_FAIL("Expect a expression" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[bo].pos));
+    if (p + 1 >= eo) EVAL_FAIL("Expect a expression" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[p + 1].pos + 1));
     word_t lhs = eval(success, bo, p), rhs, res;
     switch (op)
     {
@@ -270,7 +272,7 @@ static word_t eval(bool *success, int bo, int eo) {
     case OP_MUL : rhs = eval(success, p + 1, eo); res = lhs * rhs; break;
     case OP_DIV :
       rhs = eval(success, p + 1, eo);
-      if (rhs == 0) FAIL("eval: Divide by zero" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[p].pos));
+      if (rhs == 0) EVAL_FAIL("Divide by zero" INDICATE_FMT, INDICATE_EXPR_ARG(tokens[p].pos));
       res = lhs / rhs;
       break;
     case OP_EQ  : rhs = eval(success, p + 1, eo); res = lhs == rhs; break;
@@ -292,6 +294,6 @@ word_t expr(char *e, bool *success) {
   return eval(success, 0, nr_token);
 }
 
-#undef FAIL
+#undef EVAL_FAIL
 #undef INDICATE_FMT
 #undef INDICATE_EXPR_ARG
