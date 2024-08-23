ac1f1fc1e5cc64109871cb8fcdeea1dd0a375022
tracer-ysyx <tracer@ysyx.org>
2024-08-23 13:52:18 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:52:18 up 17:32,  2 users,  load average: 0.31, 0.22, 0.18

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -241,15 +241,15 @@ word_t eval(bool *success, int bo, int eo) {
       enum OP cur_op = token_to_op(tokens[i], may_be_unary);
       if (cur_op == OP_POS || cur_op == OP_NEG) {
         may_be_unary = true;
-      } else if (cur_op == OP_MUL || cur_op == OP_DIV) {
-        may_be_unary = true;
-        if (!paren_cnt && (p == -1 || cur_op == OP_MUL || cur_op == OP_DIV)) {
-          p = i;
-          op = cur_op;
-        }
       } else if (cur_op == OP_ADD || cur_op == OP_SUB) {
         may_be_unary = true;
         if (!paren_cnt && (p == -1 || op == OP_MUL || op == OP_DIV || op == OP_ADD || op == OP_SUB)) {
+          p = i;
+          op = cur_op;
+        }
+      } else if (cur_op == OP_MUL || cur_op == OP_DIV) {
+        may_be_unary = true;
+        if (!paren_cnt && (p == -1 || cur_op == OP_MUL || cur_op == OP_DIV)) {
           p = i;
           op = cur_op;
         }
