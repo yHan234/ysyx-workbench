05c0cc49223f34e3b7e420d1ae99f30be71e5b6c
tracer-ysyx <tracer@ysyx.org>
2024-08-23 18:17:00 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  18:17:00 up 21:45,  2 users,  load average: 0.34, 0.14, 0.11

--- nemu/src/monitor/sdb/expr.c
+++ nemu/src/monitor/sdb/expr.c
@@ -21,20 +21,7 @@
  */
 #include <regex.h>
 
-enum TK {
-  TK_NOTYPE,
-  TK_INT,
-  TK_REG,
-  TK_LPAREN,
-  TK_RPAREN,
-  TK_PLUS,
-  TK_MINUS,
-  TK_STAR,
-  TK_DIV,
-  TK_EQ,
-  TK_NE,
-  TK_ANDAND
-};
+#include "token.h"
 
 static struct rule {
   const char *regex;
---
+++ nemu/src/monitor/sdb/token.h
@@ -0,0 +1,19 @@
+#ifndef __MONITOR_SDB_TOKEN_H__
+#define __MONITOR_SDB_TOKEN_H__
+
+enum TK {
+  TK_NOTYPE,
+  TK_INT,
+  TK_REG,
+  TK_LPAREN,
+  TK_RPAREN,
+  TK_PLUS,
+  TK_MINUS,
+  TK_STAR,
+  TK_DIV,
+  TK_EQ,
+  TK_NE,
+  TK_ANDAND
+};
+
+#endif
