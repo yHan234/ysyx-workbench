8348bfd33c13371c3218eabbf24426f4c3114c79
tracer-ysyx <tracer@ysyx.org>
2024-09-11 14:34:41 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:34:41 up  2:31,  1 user,  load average: 0.11, 0.23, 0.25

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,29 +1,69 @@
-#include <nvboard.h>
 #include <Vtop.h>
 
 static Vtop dut;
 
-void nvboard_bind_all_pins(Vtop* top);
+void *img;
+long img_size;
+
+static void load_img(char *img_file) {
+  FILE *fp = fopen(img_file, "rb");
+  if (!fp) {
+    printf("Can not open '%s'", img_file);
+    exit(1);
+  }
+
+  fseek(fp, 0, SEEK_END);
+  img_size = ftell(fp);
+
+  printf("The image is %s, size = %ld", img_file, img_size);
+
+  img = malloc(img_size);
+
+  fseek(fp, 0, SEEK_SET);
+  int ret = fread(img, img_size, 1, fp);
+  assert(ret == 1);
+
+  fclose(fp);
+}
+
+uint32_t pmem_read(uint32_t pc) {
+  if (pc == 0)
+    return 0;
+
+  uint32_t img_idx = (pc - 0x80000000) / 4;
+  if (img_idx > img_size / 4) {
+    printf("image execution is complete\n");
+    exit(0);
+  }
+
+  return ((uint32_t *)img)[img_idx];
+}
 
 static void single_cycle() {
-  dut.clk = 0; dut.eval();
-  dut.clk = 1; dut.eval();
+  printf("\n\npc: %#x\n", dut.pc);
+  dut.instr = pmem_read(dut.pc);
+
+  dut.clk = 0;
+  dut.eval();
+
+  dut.clk = 1;
+  dut.eval();
 }
 
 static void reset(int n) {
   dut.rst = 1;
-  while (n -- > 0) single_cycle();
+  while (n-- > 0) {
+    single_cycle();
+  }
   dut.rst = 0;
 }
 
 int main() {
-  nvboard_bind_all_pins(&dut);
-  nvboard_init();
+  load_img("tests/addi.bin");
 
   reset(10);
 
-  while(1) {
-    nvboard_update();
+  while (true) {
     single_cycle();
   }
 }
---
+++ npc/vsrc/IDU.v
@@ -0,0 +1,47 @@
+// Instruction Decode Unit
+module IDU(
+        input [31:0] instr,
+        input [2:0] ExtOP,
+        output [6:0] op,
+        output [2:0] func3,
+        output [6:0] func7,
+        output [2:0] itype,
+        output [4:0] rd,
+        output [4:0] rs1,
+        output [4:0] rs2,
+        output [31:0] imm
+    );
+
+    assign op    = instr[6:0];
+    assign func3 = instr[14:12];
+    assign func7 = instr[31:25];
+    assign rd    = instr[11:7];
+    assign rs1   = instr[19:15];
+    assign rs2   = instr[24:20];
+
+    wire [31:0] immI, immU, immS, immB, immJ;
+    assign immI = {{20{instr[31]}}, instr[31:20]};
+    assign immU = {instr[31:12], 12'b0};
+    assign immS = {{20{instr[31]}}, instr[31:25], instr[11:7]};
+    assign immB = {{20{instr[31]}}, instr[7], instr[30:25], instr[11:8], 1'b0};
+    assign immJ = {{12{instr[31]}}, instr[19:12], instr[20], instr[30:21], 1'b0};
+
+    MuxKey #(5, 3, 32) mux_imm (
+               .out(imm),
+               .key(ExtOP),
+               .lut({
+                        3'b000, immI,
+                        3'b001, immU,
+                        3'b010, immS,
+                        3'b011, immB,
+                        3'b100, immJ
+                    })
+           );
+
+    always @(*) begin
+        $display("instr: %b", instr);
+        $display("immI: ", immI);
+        $display("imm: ", imm);
+    end
+
+endmodule
