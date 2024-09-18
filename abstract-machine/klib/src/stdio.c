a8553e02dfd4a9e12c0c62c280a45a228d3603e8
tracer-ysyx <tracer@ysyx.org>
2024-09-18 13:53:30 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:53:30 up  1:09,  1 user,  load average: 0.31, 0.19, 0.18

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -14,7 +14,53 @@ int vsprintf(char *out, const char *fmt, va_list ap) {
 }
 
 int sprintf(char *out, const char *fmt, ...) {
-  panic("Not implemented");
+  va_list args;
+  va_start(args, fmt);
+
+  const char *ptr;
+  char *out_ptr = out;
+  int written = 0;
+
+  for (ptr = fmt; *ptr != '\0'; ptr++) {
+    if (*ptr == '%' && *(ptr + 1) != '\0') {
+      ptr++;
+      switch (*ptr) {
+      case 's': {
+        const char *str = va_arg(args, const char *);
+        while (*str != '\0') {
+          *out_ptr++ = *str++;
+          written++;
+        }
+        break;
+      }
+      case 'd': {
+        int num = va_arg(args, int);
+        int len = sprintf(out_ptr, "%d", num);
+        if (len < 0) {
+          va_end(args);
+          return -1; // 输出错误
+        }
+        out_ptr += len;
+        written += len;
+        break;
+      }
+      default:
+        *out_ptr++ = '%';
+        *out_ptr++ = *ptr;
+        written += 2;
+        break;
+      }
+    } else {
+      *out_ptr++ = *ptr;
+      written++;
+    }
+  }
+
+  *out_ptr = '\0';
+
+  va_end(args);
+
+  return written;
 }
 
 int snprintf(char *out, size_t n, const char *fmt, ...) {
