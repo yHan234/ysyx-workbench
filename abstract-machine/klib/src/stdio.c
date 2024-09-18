bbee6893b50c077e154918101355a48f9fb139b6
tracer-ysyx <tracer@ysyx.org>
2024-09-18 14:13:54 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  14:13:54 up  1:29,  1 user,  load average: 0.32, 0.20, 0.14

--- abstract-machine/klib/src/stdio.c
+++ abstract-machine/klib/src/stdio.c
@@ -5,6 +5,38 @@
 
 #if !defined(__ISA_NATIVE__) || defined(__NATIVE_USE_KLIB__)
 
+static int print_int(char *out, int num) {
+  static char temp[16];
+  int i = 0;
+  int is_negative = 0;
+
+  // 处理负数
+  if (num < 0) {
+    is_negative = 1;
+    num = -num;
+  }
+
+  // 将数字转换为字符串，从末尾开始存入 temp
+  do {
+    temp[i++] = (num % 10) + '0';
+    num /= 10;
+  } while (num > 0);
+
+  // 如果是负数，添加负号
+  if (is_negative) {
+    temp[i++] = '-';
+  }
+
+  int len = i;
+
+  // 反转 temp，并存入目标缓冲区
+  while (i > 0) {
+    *(out++) = temp[--i];
+  }
+
+  return len;
+}
+
 int printf(const char *fmt, ...) {
   panic("Not implemented");
 }
@@ -35,20 +67,13 @@ int sprintf(char *out, const char *fmt, ...) {
       }
       case 'd': {
         int num = va_arg(args, int);
-        int len = sprintf(out_ptr, "%d", num);
-        if (len < 0) {
-          va_end(args);
-          return -1; // 输出错误
-        }
+        int len = print_int(out_ptr, num);
         out_ptr += len;
         written += len;
         break;
       }
       default:
-        *out_ptr++ = '%';
-        *out_ptr++ = *ptr;
-        written += 2;
-        break;
+        return -1;
       }
     } else {
       *out_ptr++ = *ptr;
