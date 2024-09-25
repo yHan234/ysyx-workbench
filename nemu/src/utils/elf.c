2d619f945a9486274d6398af1d7e33a2c954b22f
tracer-ysyx <tracer@ysyx.org>
2024-09-25 13:48:25 +0800

    > compile NEMU ysyx_22040000 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  13:48:25 up  2:15,  1 user,  load average: 0.17, 0.16, 0.15

--- nemu/src/utils/elf.c
+++ nemu/src/utils/elf.c
@@ -46,8 +46,7 @@ void init_elf(const char *elf_file) {
   char *strtab = malloc(strtab_hdr->sh_size);
   printf("strtab offset %d size %d\n", strtab_hdr->sh_offset, strtab_hdr->sh_size);
   lseek(fd, strtab_hdr->sh_offset, SEEK_SET);
-  printf("readed %ld\n", read(fd, strtab, strtab_hdr->sh_size));
-  // Assert(read(fd, strtab, strtab_hdr->sh_size) == symtab_hdr->sh_size, "Failed to read string table");
+  Assert(read(fd, strtab, strtab_hdr->sh_size) == strtab_hdr->sh_size, "Failed to read string table");
 
   int symcount = symtab_hdr->sh_size / sizeof(Elf32_Sym);
   for (int i = 0; i < symcount; i++) {
