1fd5a31210b70a67e3e199231a00020f2fe6f0f6
tracer-ysyx <tracer@ysyx.org>
2024-09-26 11:32:52 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  11:32:52 up 23 min,  1 user,  load average: 0.24, 0.22, 0.11

--- abstract-machine/klib/src/string.c
+++ abstract-machine/klib/src/string.c
@@ -56,17 +56,17 @@ char *strncat(char *dst, const char *src, size_t n) {
 }
 
 int strcmp(const char *s1, const char *s2) {
-  // while (*s1 != '\0' && *s2 != '\0') {
-  //   if (*s1 < *s2) {
-  //     return -1;
-  //   } else if (*s1 > *s2) {
-  //     return 1;
-  //   }
+  while (*s1 != '\0' && *s2 != '\0') {
+    if (*s1 < *s2) {
+      return -1;
+    } else if (*s1 > *s2) {
+      return 1;
+    }
 
-  //   // *s1 == *s2
-  //   s1 += 1;
-  //   s2 += 1;
-  // }
+    // *s1 == *s2
+    s1 += 1;
+    s2 += 1;
+  }
 
   if (*s1 != '\0') {
     return 1;
