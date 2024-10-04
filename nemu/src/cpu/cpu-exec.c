8de5456e5c91bd0d4f65a74c9f779c696dff8a93
tracer-ysyx <tracer@ysyx.org>
2024-10-04 15:28:34 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:28:34 up  7:09,  1 user,  load average: 0.27, 0.26, 0.32

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -171,6 +171,12 @@ static void exec_once(Decode *s, vaddr_t pc) {
   s->snpc = pc;
   isa_exec_once(s);
   cpu.pc = s->dnpc;
+
+  printf("ref reg: ");
+  for (int i = 0; i < 32; ++i) {
+    printf("%d ", cpu.gpr[i]);
+  }
+  printf("\n");
 }
 
 static void execute(uint64_t n) {
