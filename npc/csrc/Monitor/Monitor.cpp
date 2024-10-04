a433615fa830c263caf6a5e6cdd3983b55a3bf9a
tracer-ysyx <tracer@ysyx.org>
2024-10-04 15:32:04 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:32:04 up  7:12,  1 user,  load average: 0.54, 0.30, 0.31

--- npc/csrc/Monitor/Monitor.cpp
+++ npc/csrc/Monitor/Monitor.cpp
@@ -150,6 +150,7 @@ void Monitor::DiffTestStep() {
   auto &dut_regs = cpu.GetRegs();
   auto dut_pc = cpu.GetPC();
 
+  std::cout << pc << '\n';
   for (int i = 0; i < 32; ++i)
     std::cout << ref_regs[i] << ' ';
   std::cout << '\n';
