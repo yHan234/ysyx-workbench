ceddafd83fc92945b9c5726b1dea2cd0afb4e66c
tracer-ysyx <tracer@ysyx.org>
2024-10-04 15:50:07 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  15:50:07 up  7:30,  1 user,  load average: 0.54, 0.33, 0.28

--- npc/vsrc/top.v
+++ npc/vsrc/top.v
@@ -21,7 +21,7 @@ module top(
             .dout ( pc     ),
             .wen  ( 1'b1   )
         );
-    always @(*) $display("pc imm: ", pc, imm);
+    always @(*) $display("pc imm: ", pc, " ", imm);
     assign NextPC = (PCAsrc == 0 ? 4 : imm) + (PCBsrc == 0 ? pc : rbus1);
 
     // GPR
