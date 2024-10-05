16bd17a10302c48e2cd916bf5695a8d34dc2d428
tracer-ysyx <tracer@ysyx.org>
2024-10-06 01:38:52 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  01:38:52 up  1:18,  1 user,  load average: 0.47, 0.37, 0.22

--- nemu/src/device/timer.c
+++ nemu/src/device/timer.c
@@ -21,8 +21,8 @@ static uint32_t *rtc_port_base = NULL;
 
 static void rtc_io_handler(uint32_t offset, int len, bool is_write) {
   assert(offset == 0 || offset == 4);
-  printf("offset: %d\n", offset);
-  if (!is_write && offset == 4) {
+  // printf("offset: %d\n", offset);
+  if (!is_write) {
     uint64_t us = get_time();
     *(uint64_t *)rtc_port_base = us;
     printf("nemu us: %lx\n", us);
