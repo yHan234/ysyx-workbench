8e3d5ae0d6b1fc77d5d363833533ea39f17e2fc1
tracer-ysyx <tracer@ysyx.org>
2024-10-06 20:20:30 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:20:30 up  5:26,  1 user,  load average: 0.29, 0.30, 0.24

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -38,6 +38,7 @@ static uint32_t *vgactl_port_base = NULL;
 #ifndef CONFIG_TARGET_AM
 #include <SDL2/SDL.h>
 
+// static SDL_Window *window = NULL;
 static SDL_Renderer *renderer = NULL;
 static SDL_Texture *texture = NULL;
 
@@ -54,7 +55,7 @@ static void init_screen() {
   texture = SDL_CreateTexture(renderer, SDL_PIXELFORMAT_ARGB8888,
       SDL_TEXTUREACCESS_STATIC, SCREEN_W, SCREEN_H);
   SDL_RenderPresent(renderer);
-  SDL_DestroyWindow(window);
+  // SDL_DestroyWindow(window);
 }
 
 static inline void update_screen() {
