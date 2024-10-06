6c6716d2bb45c47643ee2b24d08be8896f46392f
YangHan <yhan234@qq.com>
2024-10-06 19:36:29 +0800

    IOE 实现 vga

--- nemu/src/device/vga.c
+++ nemu/src/device/vga.c
@@ -71,18 +71,19 @@ static inline void update_screen() {
 #endif
 #endif
 
-void vga_update_screen() {
-  // TODO: call `update_screen()` when the sync register is non-zero,
-  // then zero out the sync register
+void vga_update_screen(uint32_t offset, int len, bool is_write) {
+  if (offset == 4 && is_write && vgactl_port_base[1]) {
+    update_screen();
+  }
 }
 
 void init_vga() {
   vgactl_port_base = (uint32_t *)new_space(8);
   vgactl_port_base[0] = (screen_width() << 16) | screen_height();
 #ifdef CONFIG_HAS_PORT_IO
-  add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, NULL);
+  add_pio_map ("vgactl", CONFIG_VGA_CTL_PORT, vgactl_port_base, 8, vga_update_screen);
 #else
-  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, NULL);
+  add_mmio_map("vgactl", CONFIG_VGA_CTL_MMIO, vgactl_port_base, 8, vga_update_screen);
 #endif
 
   vmem = new_space(screen_size());
