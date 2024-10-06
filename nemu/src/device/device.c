13a7995ede3799dca0f4ed8999dada928947765a
tracer-ysyx <tracer@ysyx.org>
2024-10-06 17:58:15 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  17:58:15 up  3:04,  1 user,  load average: 1.55, 0.96, 0.53

--- nemu/src/device/device.c
+++ nemu/src/device/device.c
@@ -41,7 +41,7 @@ void device_update() {
   }
   last = now;
 
-  // IFDEF(CONFIG_HAS_VGA, vga_update_screen());
+  IFDEF(CONFIG_HAS_VGA, vga_update_screen());
 
 #ifndef CONFIG_TARGET_AM
   SDL_Event event;
