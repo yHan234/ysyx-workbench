361bb05f5847ffe625faf49ebf874ae8e0d96ea5
tracer-ysyx <tracer@ysyx.org>
2024-10-07 19:22:33 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  19:22:33 up  6:17,  1 user,  load average: 0.40, 0.51, 0.68

--- npc/csrc/Monitor/Monitor.cpp
+++ npc/csrc/Monitor/Monitor.cpp
@@ -26,7 +26,6 @@ Monitor::Monitor(CPU &cpu, MemoryManager &mem_mgr)
 
   cpu.before_step = [&]() -> int {
     pc = cpu.GetPC(); // 本次执行的 PC
-    std::cerr << pc << std::endl;
     return 0;
   };
 
