5e930064ff3eafcb0eb4453de7d8a9048a37eaac
tracer-ysyx <tracer@ysyx.org>
2024-10-07 23:12:32 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:12:32 up 10:07,  1 user,  load average: 0.58, 0.29, 0.27

--- npc/vsrc/DataMem.v
+++ npc/vsrc/DataMem.v
@@ -104,7 +104,7 @@ MuxKey #(3, 2, 40) mux_mem_write_byte2(
     })
 );
 
-always @(*) begin
+always @(posedge RdClk) begin
     if (MemRd) begin
         pmem_read(aligned_addr, read);
     end else begin
