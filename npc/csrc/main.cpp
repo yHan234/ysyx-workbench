84b02319918f57a2e9898318aeb4a9bdd66ba9e8
tracer-ysyx <tracer@ysyx.org>
2024-10-07 22:33:45 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  22:33:45 up  9:28,  1 user,  load average: 0.35, 0.32, 0.27

--- npc/csrc/main.cpp
+++ npc/csrc/main.cpp
@@ -1,5 +1,6 @@
 #include "CPU/CPU.hpp"
 #include "Debugger/Debugger.hpp"
+#include "Device/devices.hpp"
 #include "Memory/Memory.hpp"
 #include "Monitor/Monitor.hpp"
 #include "Utils/argparse.hpp"
@@ -9,6 +10,7 @@
 
 MemoryManager mem_mgr;
 Memory mem(mem_mgr);
+Serial serial(mem_mgr);
 CPU cpu;
 Monitor monitor(cpu, mem_mgr);
 Debugger dbg(cpu, mem_mgr, monitor);
