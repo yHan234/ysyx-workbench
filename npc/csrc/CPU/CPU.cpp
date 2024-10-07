991d3ecc19f8bc110acc66f99d6dd8bc598961c3
tracer-ysyx <tracer@ysyx.org>
2024-10-07 20:14:58 +0800

    > sim RTL ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  20:14:58 up  7:10,  1 user,  load average: 0.85, 0.49, 0.37

--- npc/csrc/CPU/CPU.cpp
+++ npc/csrc/CPU/CPU.cpp
@@ -4,8 +4,8 @@ void CPU::SingleCycle() {
   dut.clk = 0;
     std::cerr << dut.clk << std::endl;
   dut.eval();
-    std::cerr << dut.clk << std::endl;
   dut.clk = 1;
+    std::cerr << dut.clk << std::endl;
   dut.eval();
 }
 
