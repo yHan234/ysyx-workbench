5cd93a390038e8ffea512dda750b1ec0eb4ab42d
tracer-ysyx <tracer@ysyx.org>
2024-10-09 21:56:17 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  21:56:17 up  2:46,  1 user,  load average: 0.28, 0.30, 0.23

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -8,8 +8,8 @@ Context* __am_irq_handle(Context *c) {
   if (user_handler) {
     Event ev = {0};
     switch (c->mcause) {
-      case 0xb: ev.event = EVENT_YIELD; c->mepc += 4; break;
-      default:  ev.event = EVENT_ERROR;               break;
+      case 0xb: ev.event = EVENT_YIELD; break;
+      default:  ev.event = EVENT_ERROR; break;
     }
 
     c = user_handler(ev, c);
@@ -37,6 +37,7 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   // sp 指向 Context 末尾
   Context *p = (Context *)kstack.start;
   p->mepc = (uintptr_t)entry;
+  printf("mepc %x\n", entry);
   p->gpr[2] /*sp*/ = (uintptr_t)p + sizeof(Context);
   return p;
 }
