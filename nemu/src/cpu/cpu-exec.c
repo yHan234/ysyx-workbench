af06f2acc7fc7364627cd4035421534f3b3e1117
tracer-ysyx <tracer@ysyx.org>
2024-10-10 00:01:55 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  00:01:55 up  4:51,  1 user,  load average: 0.27, 0.18, 0.13

--- nemu/src/cpu/cpu-exec.c
+++ nemu/src/cpu/cpu-exec.c
@@ -181,6 +181,7 @@ static void execute(uint64_t n) {
     trace_and_difftest(&s, cpu.pc);
     if (nemu_state.state != NEMU_RUNNING) break;
     IFDEF(CONFIG_DEVICE, device_update());
+    printf("%x: %d", cpu.pc, cpu.csr[CSR_MSTATUS]);
   }
 }
 
