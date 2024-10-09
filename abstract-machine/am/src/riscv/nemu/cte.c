714cc0d57face3e48c1e314719a14ed36d6600de
tracer-ysyx <tracer@ysyx.org>
2024-10-09 23:40:40 +0800

    > compile NEMU ysyx_24080026 杨晗 Linux YangHan 5.15.153.1-microsoft-standard-WSL2 #1 SMP Fri Mar 29 23:14:13 UTC 2024 x86_64 x86_64 x86_64 GNU/Linux  23:40:40 up  4:30,  1 user,  load average: 0.28, 0.15, 0.11

--- abstract-machine/am/src/riscv/nemu/cte.c
+++ abstract-machine/am/src/riscv/nemu/cte.c
@@ -38,9 +38,9 @@ Context *kcontext(Area kstack, void (*entry)(void *), void *arg) {
   // kstack 中写入一个新的 Context
   // 其中 mepc 为 entry
   // sp 指向 Context 末尾
-  Context *p = (Context *)kstack.start;
+  Context *p = kstack.end - sizeof(Context);
   p->mepc = (uintptr_t)entry;
-  p->gpr[2] /*sp*/ = (uintptr_t)p + sizeof(Context);
+  p->gpr[2] /*sp*/ = (uintptr_t)p;
   return p;
 }
 
