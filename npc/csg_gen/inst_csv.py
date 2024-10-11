0c5a3f571977821c6ed85a5229412fb0abe36fc6
YangHan <yhan234@qq.com>
2024-10-11 22:20:52 +0800

    csg gen 动态根据 csv 的字段进行生成

--- npc/csg_gen/inst_csv.py
+++ npc/csg_gen/inst_csv.py
@@ -1,76 +1,68 @@
 import csv
 
 
-class Inst:
+class InstCSV:
+    """
+    CSV 格式：
+
+    row1: name, explain, op[6:2], funct3, funct7[5], {要生成的信号...}
+
+    row2: ,,,,,{要生成的信号的长度...}
+
+    rows...
+    """
+
+    class Signal:
+        def __init__(self, name, len):
+            self.name = name
+            self.len = len
+
+    class Inst:
+        def __init__(self, op, funct3, funct7, signals):
+            self.op = op
+            self.funct3 = funct3
+            self.funct7 = funct7
+            self.signals = signals
+
+        def sig_str(self):
+            return "".join(self.signals)
+
     def __init__(self):
-        self.name = None
-        self.op = None
-        self.funct3 = None
-        self.funct7 = None
-        self.inst_type = None
-        self.reg_wr_en = None
-        self.branch = None
-        self.mem2reg = None
-        self.mem_rd_en = None
-        self.mem_wr_en = None
-        self.mem_op = None
-        self.alu_src_a = None
-        self.alu_src_b = None
-        self.alu_ctr = None
-        self.explain = None
+        self.signals = None
+        self.insts = None
+        self.sig_tot_len = None
 
-    def res(self) -> str:
-        return (
-            self.inst_type
-            + self.reg_wr_en
-            + self.branch
-            + self.mem2reg
-            + self.mem_rd_en
-            + self.mem_wr_en
-            + self.mem_op
-            + self.alu_src_a
-            + self.alu_src_b
-            + self.alu_ctr
-        )
+    def load(path: str):
+        icsv = InstCSV
+        with open(path) as f:
+            reader = csv.reader(f)
 
+            header = next(reader)
+            assert header[0:5] == [
+                "name",
+                "explain",
+                "op[6:2]",
+                "funct3",
+                "funct7[5]",
+            ]
+            lengths = [None if s == "" else int(s) for s in next(reader)]
 
-def load_insts_from_csv(path: str):
-    insts = []
-    with open(path) as f:
-        reader = csv.reader(f)
-        header = next(reader)
-        assert header == [
-            "name",
-            "op[6:2]",
-            "funct3",
-            "funct7[5]",
-            "inst_type",
-            "reg_wr_en",
-            "branch",
-            "mem2reg",
-            "mem_rd_en",
-            "mem_wr_en",
-            "mem_op",
-            "alu_src_a",
-            "alu_src_b",
-            "alu_ctr",
-            "explain",
-        ]
-        for row in reader:
-            insts.append(Inst())
-            insts[-1].name = row[0]
-            insts[-1].op = None if row[1] == "" else row[1]
-            insts[-1].funct3 = None if row[2] == "" else row[2]
-            insts[-1].funct7 = None if row[3] == "" else row[3]
-            insts[-1].inst_type = row[4].zfill(3)
-            insts[-1].reg_wr_en = row[5].zfill(1)
-            insts[-1].branch = row[6].zfill(3)
-            insts[-1].mem2reg = row[7].zfill(1)
-            insts[-1].mem_rd_en = row[8].zfill(1)
-            insts[-1].mem_wr_en = row[9].zfill(1)
-            insts[-1].mem_op = row[10].zfill(3)
-            insts[-1].alu_src_a = row[11].zfill(1)
-            insts[-1].alu_src_b = row[12].zfill(2)
-            insts[-1].alu_ctr = row[13].zfill(4)
-            insts[-1].explain = row[14]
-    return insts
+            icsv.signals = [
+                InstCSV.Signal(header[i], lengths[i]) for i in range(5, len(header))
+            ]
+            icsv.sig_tot_len = sum([s.len for s in icsv.signals])
+
+            icsv.insts = [
+                InstCSV.Inst(
+                    inst[2],  # op
+                    None if inst[3] == "" else inst[3],  # funct3
+                    None if inst[4] == "" else inst[4],  # funct7
+                    [
+                        inst[i].zfill(lengths[i])  # 用 0 填充各个信号
+                        for i in range(5, len(inst))
+                    ],
+                )
+                for inst in reader
+            ]
+
+        return icsv
