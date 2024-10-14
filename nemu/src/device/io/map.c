86bb418496b540bf431aa3bdd1cedb83ae49d1d8
YangHan <yhan234@qq.com>
2024-10-14 15:02:31 +0800

    nemu 访存错误不直接退出，而是设置 state 为 abort

--- nemu/src/device/io/map.c
+++ nemu/src/device/io/map.c
@@ -32,14 +32,18 @@ uint8_t* new_space(int size) {
   return p;
 }
 
-static void check_bound(IOMap *map, paddr_t addr) {
+static bool check_bound(IOMap *map, paddr_t addr) {
   if (map == NULL) {
-    Assert(map != NULL, "address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
-  } else {
-    Assert(addr <= map->high && addr >= map->low,
-        "address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
+    Log("address (" FMT_PADDR ") is out of bound at pc = " FMT_WORD, addr, cpu.pc);
+    nemu_state.state = NEMU_ABORT;
+    return false;
+  } else if (!(addr <= map->high && addr >= map->low)) {
+    Log("address (" FMT_PADDR ") is out of bound {%s} [" FMT_PADDR ", " FMT_PADDR "] at pc = " FMT_WORD,
         addr, map->name, map->low, map->high, cpu.pc);
+    nemu_state.state = NEMU_ABORT;
+    return false;
   }
+  return true;
 }
 
 static void invoke_callback(io_callback_t c, paddr_t offset, int len, bool is_write) {
@@ -54,7 +58,7 @@ void init_map() {
 
 word_t map_read(paddr_t addr, int len, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
+  if (!check_bound(map, addr)) return 0;
   paddr_t offset = addr - map->low;
   invoke_callback(map->callback, offset, len, false); // prepare data to read
   word_t ret = host_read(map->space + offset, len);
@@ -63,7 +67,7 @@ word_t map_read(paddr_t addr, int len, IOMap *map) {
 
 void map_write(paddr_t addr, int len, word_t data, IOMap *map) {
   assert(len >= 1 && len <= 8);
-  check_bound(map, addr);
+  if (!check_bound(map, addr)) return;
   paddr_t offset = addr - map->low;
   host_write(map->space + offset, len, data);
   invoke_callback(map->callback, offset, len, true);
